f640d3f9b72b140aa84f41657bb063b28cb1bc83
Rick <rick@nexassist.com>
2026-03-14 22:44:29 -0700

    Add Google Analytics G-MB40V3YQVZ to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import WhatsAppBubble from "@/components/WhatsAppBubble";
 
@@ -197,6 +198,19 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-[#080d18] text-white font-inter antialiased overflow-x-hidden">
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MB40V3YQVZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MB40V3YQVZ');
+          `}
+        </Script>
         {children}
         <WhatsAppBubble />
       </body>
